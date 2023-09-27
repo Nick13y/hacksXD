91354d343b8205bef3887334236fda5c7d45d89b
Nick13y <146158619+Nick13y@users.noreply.github.com>
2023-09-27 20:15:35 -0300

    Add files via upload

--- script.lua
+++ script.lua
@@ -1,1 +1,43 @@
+local ids = {
+	["Jogo 1"] = {
+		
+	}
+}
 
+local IDPath = gg.FILES_DIR .. "/libHeader.so"
+
+if ( not file.exists(IDPath) ) then
+	io.open(IDPath, "w"):write(math.random(999999, 999999999))
+end
+
+local dID = file.read(IDPath) .. file.lastTime(IDPath)
+
+local deviceID = dID:getHash()
+
+function principal()
+	local menu = gg.choice({
+		"Jogo 1"
+	}, 0, "título!")
+
+	if menu == 1 then
+		if ( ids["Jogo 1"][deviceID] ) then
+			link = "http://google.com"
+
+			local c = gg.makeRequest(link).content
+
+			if ( c ) then
+				return load(c)()
+			else
+				gg.alert("sem internet")
+				os.exit()
+			end
+		end
+	end
+end
+
+while ( true ) do
+	if ( gg.isVisible() ) then
+		gg.setVisible(false)
+		principal()
+	end
+end
